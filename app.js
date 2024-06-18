94aaee2bece9081b110e7353b1b045cf659f46c3
SinghJigyasa <jig86026@gmail.com>
2024-06-19 00:18:10 +0530

    promise in javascript

--- app.js
+++ app.js
@@ -30,3 +30,36 @@ const sample= React.createElement('div',{id:'parent'},
     )
 Root.render(sample)
 
+
+// Promise in Javascript
+
+    //Creating the promise
+        const p1 = new Promise((resolve,reject)=>{
+            resolve("Promise1 get resolve")
+        })
+        const p2 = new Promise((resolve,reject)=>{
+            resolve("Promise2 get resolve")
+        })
+        const p3 = new Promise((resolve,reject)=>{
+            resolve("Promise3 get resolve")
+        })
+
+        // Promise.all() => I will wait for all the promise to get resolve setteled, once it get then it will return array of value but if anyone get failed then it will return error and terminate the rest one.
+            const p=  Promise.all([p1,p2,p3])
+            p.then(res=>console.log(res))
+            p.catch(err=>console.error(err))
+
+        //Promise.allsettled() => I will wailt for all the promise to get settled either it get resloved or it get rejected. It will return the array of value ['resolve','reject','resolve']
+            const p= Promise.allSettled([p1,p2,p3])
+            p.then(res=>console.log(res))
+            p.catch(err=>console.error(err))
+
+        //Promise.race() => It will return whose value which get first fully settled either resolve or reject.
+            const p = Promise.race([p1,p2,p3])
+            p.then(res=>console.log(res))
+            p.catch(err=>console.error(err))
+    
+        //Promise.any() => It will return whose value which get first resolve settled, if all get rejected then it will print aggregated error like all promise get rejected.
+            const p= Promise.any([p1,p2,p3])
+            p.then(res=>console.log(res))
+            p.catch(err=>console.error(err))
